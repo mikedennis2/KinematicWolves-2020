a8c0033d54b9496bc789797492291f36567ac435
kinematicwolves <54362016+kinematicwolves@users.noreply.github.com>
2020-02-24 17:50:27 -0500

    CAN IDs for rest of the system added

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -64,13 +64,13 @@ public final class Constants {
 
     // This does not exist yet!
     // public static final int ROTATE_TURRET_MOTOR = 10; // Does not exist
-    public static final int TOP_CONVEYOR_MOTOR = 26;
-    public static final int LOWER_CONVEYOR_MOTOR = 27;
-    public static final int INTAKE_MOTOR = 28;
+    public static final int TOP_CONVEYOR_MOTOR = 1;
+    public static final int LOWER_CONVEYOR_MOTOR = 4;
+    public static final int INTAKE_MOTOR = 7;
    
     // Shooter controller CAN IDs
-    public static final int SPIN_SHOOTER_MOTOR_1 = 12;
-    public static final int SPIN_SHOOTER_MOTOR_2 = 13;
+    public static final int SPIN_SHOOTER_MOTOR_1 = 2;
+    public static final int SPIN_SHOOTER_MOTOR_2 = 3;
 
     // PWM channels for servo controller linear actuators
     public static final int LINEAR_ACTUATOR_1 = 1;
